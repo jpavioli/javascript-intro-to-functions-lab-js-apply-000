4e0a29ddcaef35f98ce86eb4e0ad4afe3dee8a7c
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-05 16:44:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+//recieve a [string] and return it in all caps
+function shout(string){
+  string.toUpperCase()
+  return string
+}
+
+//recieve a [string] and return it in all lower case
+function whisper(string){
+  string.toLowerCase()
+  return string
+}
+
